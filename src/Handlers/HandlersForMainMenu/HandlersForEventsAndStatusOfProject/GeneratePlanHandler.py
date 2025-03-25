563f0726792ed5be9c15042ed2ad33805803d0f3
Aleksey Salamatin <asalamatin640@gmail.com>
2025-03-26 03:20:55 +0500

    :)

--- src/Handlers/HandlersForMainMenu/HandlersForEventsAndStatusOfProject/GeneratePlanHandler.py
+++ src/Handlers/HandlersForMainMenu/HandlersForEventsAndStatusOfProject/GeneratePlanHandler.py
@@ -10,8 +10,8 @@ class GeneratePlanHandler(Handler):
     async def handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
         proj_id = 2 #context.user_data["chosenProject"]
 
-        iam_token = "t1.9euelZqJzZGTj5GKk8uJjZiXjp6Wnu3rnpWanoyPkpnJzs2TmcmUz8fGncvl8_cYO3VA-e8LMm9__d3z91hpckD57wsyb3_9zef1656Vms-Lz8mdl83Ni5iYmImei8aX7_zF656Vms-Lz8mdl83Ni5iYmImei8aX.oTJ4RkqpLLR01-7si8QYo6IbbWaSswacw8_c9sy-VUXJe0XFxOsMW7QiQkvFd9q9SkPJDzgBLMAChj6UVd7rBg"
-        folder_id = "b1gc80aslek1slbmcvj9"
+        iam_token = ""
+        folder_id = ""
 
         MAX_MESSAGE_LENGTH = 4096  # Максимальная длина сообщения в Telegram
 
